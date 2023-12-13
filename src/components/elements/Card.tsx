dd1e18bb6224d0d19ec0db681fc9b45903277f55
AkiraDev <akira.devjs@gmail.com>
2023-12-13 13:12:32 -0800

    initial commit

---
+++ src/components/elements/Card.tsx
@@ -0,0 +1,32 @@
+import { component$, Slot } from '@builder.io/qwik';
+import { LogoGithub, GlobeOutline } from 'qwik-ionicons';
+
+export default component$(({ link, name, icon, github }: any) => {
+  return (
+    <div class="flex-1 relative bg-gray-800 border border-gray-700 rounded-xl group transition-all">
+      <div class="grid rounded-xl absolute w-full h-full bg-gray-900/50 transition opacity-0 group-hover:opacity-100 group-hover:backdrop-blur-sm">
+        {link && (
+          <a href={link} class="flex flex-col justify-center transition hover:bg-luminescent-500/20 text-white rounded-xl font-bold items-center gap-4">
+            <GlobeOutline width="24" class="fill-current" />
+            Visit page
+          </a>
+        )}
+        {github && (
+          <a href={github} class="flex flex-col justify-center transition hover:bg-luminescent-500/20 text-white rounded-xl font-bold items-center gap-4">
+            <LogoGithub width="24" class="fill-current" />
+            Github
+          </a>
+        )}
+      </div>
+      <div class="p-8 w-64">
+        {icon && (
+        <img src={icon} alt={name} class="mb-10 rounded-lg" draggable={false} width={192} height={192} />
+        )}
+        <h2 class="text-white text-xl font-bold">{name}</h2>
+        <p class="text-gray-400 mb-2">
+          <Slot />
+        </p>
+      </div>
+    </div>
+  );
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,97 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
-
-import Counter from "~/components/starter/counter/counter";
-import Hero from "~/components/starter/hero/hero";
-import Infobox from "~/components/starter/infobox/infobox";
-import Starter from "~/components/starter/next-steps/next-steps";
+import { type DocumentHead } from "@builder.io/qwik-city";
+import Card from "~/components/elements/Card";
 
 export default component$(() => {
   return (
-    <>
-      <Hero />
-      <Starter />
+    <div>
+      <header class="text-bold text-white text-center py-4" style={{ minHeight: 'calc(50dvh - 64px)' }}>
+        <h1 class="font-bold text-2xl sm:text-5xl">
+          AkiraDev
+        </h1>
+        <br />
+        <p class="flex gap-1.5 justify-center items-center">
+          <img width="170" height="28" src="https://api.statusbadges.me/badge/status/798738506859282482?simple=true&style=for-the-badge" alt="status" />
+          <img width="136" height="28" src="https://api.statusbadges.me/badge/playing/798738506859282482?style=for-the-badge" alt="playing" />
+          <img width="183" height="28" src="https://api.statusbadges.me/badge/vscode/798738506859282482?style=for-the-badge" alt="vscode" />
+          <a href="https://api.statusbadges.me/openspotify/798738506859282482" target="_blank" rel="noopener"><img width="301" height="28" src="https://api.statusbadges.me/badge/spotify/798738506859282482?style=for-the-badge" alt="spotify" /></a>
+        </p>
+      </header>
 
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
+      <section class="flex flex-col mx-auto max-w-6xl px-6 items-center justify-center" >
+        <h1 class="font-bold text-white text-xl sm:text-3xl mt-10">
+          Main Projects
+        </h1>
+        <h1 class="text-white text-sm sm:text-xl pb-8">
+          Here are some of the projects I work on
+        </h1>
+        <div class="flex w-full overflow-x-hidden" style={{
+          '-webkit-mask-image': 'linear-gradient(90deg, transparent, #000 2%, #000 98%, transparent);',
+        }}>
+          <div class="flex content-center justify-center gap-4 px-10 overflow-x-auto py-2 mx-auto">
+            <Card name="SimplyMC" icon="https://www.simplymc.art/icon-192x192.png" github="https://github.com/LuminescentDev/SimplyMC" link="https://simplymc.art">
+              A Minecraft Multitool for server owners, developers, and players.
+            </Card>
+            <Card name="Lumin" icon="https://lumin.luminescent.dev/Lumin.png" github="https://github.com/Oli-idk/Lumin" link="https://lumin.luminescent.dev/">
+              A silly little moon themed bot<br />
+              Interactions, Utilities, Fun!
+            </Card>
+            <Card name="Bwmps Tools" icon="/images/IMG_7096.png" link="https://shop.akiradev.me/b/VCP7a">
+              A multipurpose unity tool for vrchat avatar creation
+            </Card>
           </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
-
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
         </div>
-      </div>
-    </>
+      </section>
+      <section class="flex flex-col mx-auto max-w-6xl px-6 items-center" >
+        <h1 class="font-bold text-white text-xl sm:text-3xl mt-10">
+          All Projects
+        </h1>
+        <h1 class="text-white text-sm sm:text-xl pb-8">
+          ALL MY STUFF
+        </h1>
+        <div class="flex w-full overflow-x-hidden" style={{
+          '-webkit-mask-image': 'linear-gradient(90deg, transparent, #000 2%, #000 98%, transparent);',
+        }}>
+          <div class="flex content-center justify-center gap-4 px-10 overflow-x-auto py-2 mx-auto">
+            <Card name="DeezItems" icon="/images/DeezItems.jpg" github="https://github.com/LuminescentDev/DeezItems" link="https://www.spigotmc.org/resources/deezitems.103356/">
+              Custom Item plugin that allows server owners to create custom items with special abilities
+            </Card>
+            <Card name="Player Perks" icon="/images/IMG_7096.png" github="https://github.com/LuminescentDev/PlayerPerks">
+              Custom perk plugin i made for my friend's server
+            </Card>
+          </div>
+        </div>
+      </section>
+      <section class="flex flex-col mx-auto max-w-6xl px-6 items-center" >
+        <h1 class="font-bold text-white text-xl sm:text-3xl mt-10">
+          Discontinued Projects
+        </h1>
+        <h1 class="text-white text-sm sm:text-xl pb-8">
+          Random projects that i have stopped working on for one reason or another
+        </h1>
+        <div class="flex w-full overflow-x-hidden" style={{
+          '-webkit-mask-image': 'linear-gradient(90deg, transparent, #000 2%, #000 98%, transparent);',
+        }}>
+          <div class="flex content-center justify-center gap-4 px-10 overflow-x-auto py-2 mx-auto">
+            <Card name="Aether SMP" icon="/images/Aether.jpg" link="https://www.aethersmp.com/">
+              My old minecraft server that died due to lack of funding.
+            </Card>
+            <Card name="Sabotage" icon="/images/Sabotage.png" github="https://github.com/LuminescentDev/Sabotage">
+              Discontinued for the same reason as aether, old gamemode server that never got any traction. It still remains as an open source plugin for anyone to use.
+            </Card>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "AkiraDev",
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
+      name: "AkiraDev",
+      content: "My Personal Website",
     },
   ],
 };
